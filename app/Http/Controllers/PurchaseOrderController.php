ba46409e95ee1464230223494f22f241cec4733a
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-07 18:26:58 +0500

    Update PurchaseOrderController.php

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -223,7 +223,7 @@ class PurchaseOrderController extends Controller
 
         $logoPath = public_path('assets/img/vistex-logo.png');
         if (file_exists($logoPath)) {
-            $pdf->Image($logoPath, 12, 8, 30);
+            $pdf->Image($logoPath, 6, 8, 60);
         }
 
         $pdf->SetFont('helvetica', 'B', 18);
