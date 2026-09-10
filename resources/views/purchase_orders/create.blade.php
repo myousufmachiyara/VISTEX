f54153d4a1a19b1e4157a15eaf033d81e5328f77
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-10 15:51:13 +0500

    Update create.blade.php

--- resources/views/purchase_orders/create.blade.php
+++ resources/views/purchase_orders/create.blade.php
@@ -221,7 +221,7 @@
                 <tr><td>Reed Space</td><td id="p_reed_space">—</td></tr>
                 <tr><td>Warp Consumption (lbs/m)</td><td id="p_warp_consumption">—</td></tr>
                 <tr><td>Weft Consumption (lbs/m)</td><td id="p_weft_consumption">—</td></tr>
-                <tr><td><strong>Total Yarn Weight Consumed (lbs)</strong></td><td id="p_total_yarn_weight_consumed">—</td></tr>
+                <tr><td><strong>Total Yarn Consumption (lbs)</strong></td><td id="p_total_yarn_weight_consumed">—</td></tr>
                 <tr><td>Warp Yarn Rate (Rs/m)</td><td id="p_warp_yarn_rate">—</td></tr>
                 <tr><td>Weft Yarn Rate (Rs/m)</td><td id="p_weft_yarn_rate">—</td></tr>
                 <tr><td><strong>Total Yarn Cost per Meter</strong></td><td id="p_total_yarn_cost_per_meter">—</td></tr>
