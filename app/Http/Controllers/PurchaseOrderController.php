dac69df3dc1c819a338a978109622b6b7d3893e1
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-10 20:00:08 +0500

    Update PurchaseOrderController.php

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -502,32 +502,35 @@ class PurchaseOrderController extends Controller
         $pdf->writeHTML($detailsHtml, true, false, false, false, '');
         $pdf->Ln(4);
 
-        $specsHtml = '
-        <table cellpadding="4" cellspacing="0" width="100%" style="border:1px solid #333; font-size:10px;">
-        <tr style="font-weight:bold; background-color:#f0f0f0;">
-            <td colspan="6">ITEM DESCRIPTION — ' . e($order->item_name ?? '') . '</td>
-            // show total meters at right corner
-        </tr>
-        <tr>
-            <td width="16%"><b>Warp Wt.</b><br>' . e($order->warpProduct->name ?? '-') . '</td>
-            <td width="16%"><b>Weft Wt.</b><br>' . e($order->weftProduct->name ?? '-') . '</td>
-            <td width="16%"><b>Total Wt.</b><br>' . e($order->warp_count) . '</td>
-        </tr>
-        <tr>
-            <td width="16%"><b>Warp Required (Lbs)</b><br>' . e($order->weft_count) . '</td>
-            <td width="16%"><b>Weft Required (Lbs)</b><br>' . e($order->weft_count) . '</td>
-            <td><b>Total lbs Required</b><br>' . number_format($order->total_meters_required, 3) . '</td>
-        </tr>
-        <tr>
-            <td><b>Rate / pick</b><br>' . number_format($order->total_meters_required, 3) . '</td>
-            <td><b>Sizing / lbs</b><br>' . number_format($order->total_meters_required, 3) . '</td>
-            <td><b>Total Weaving Rate</b><br>' . number_format($order->total_meters_required, 3) . '</td>
-        </tr>
-        <tr>
-            <td><b>GSM (Kg)</b><br>' . number_format($order->gsm, 2) . '</td>
-            <td><b>GSM Kg/meter</b><br>' . e($order->greigeProduct->name ?? 'Not specified') . '</td>
-        </tr>
-        </table>';
+$specsHtml = '
+<table cellpadding="4" cellspacing="0" width="100%" style="border:1px solid #333; font-size:10px;">
+<tr style="font-weight:bold; background-color:#f0f0f0;">
+    <td colspan="4">ITEM DESCRIPTION — ' . e($order->item_name ?? '') . '</td>
+    <td colspan="2" style="text-align:right;">Total Meters: ' . number_format($order->total_meters_required, 3) . '</td>
+</tr>
+<tr>
+    <td width="16%"><b>Warp Yarn</b><br>' . e($order->warpProduct->name ?? '-') . '</td>
+    <td width="16%"><b>Weft Yarn</b><br>' . e($order->weftProduct->name ?? '-') . '</td>
+    <td width="16%"><b>Warp Wt. (lbs/m)</b><br>' . number_format($order->warp_consumption, 4) . '</td>
+    <td width="16%"><b>Weft Wt. (lbs/m)</b><br>' . number_format($order->weft_consumption, 4) . '</td>
+    <td colspan="2" width="36%"><b>Total Wt. (lbs/m)</b><br>' . number_format($order->total_yarn_weight_consumed, 4) . '</td>
+</tr>
+<tr>
+    <td><b>Warp Required (Lbs)</b><br>' . number_format($order->warp_consumption * $order->total_meters_required, 3) . '</td>
+    <td><b>Weft Required (Lbs)</b><br>' . number_format($order->weft_consumption * $order->total_meters_required, 3) . '</td>
+    <td colspan="4"><b>Total Lbs Required</b><br>' . number_format($order->total_yarn_weight_consumed * $order->total_meters_required, 3) . '</td>
+</tr>
+<tr>
+    <td><b>Rate / Pick</b><br>' . number_format($order->rate_per_pick, 4) . '</td>
+    <td><b>Sizing (lbs)</b><br>' . number_format($order->sizing_lbs ?? 0, 4) . '</td>
+    <td colspan="4"><b>Total Weaving Rate (Rs/m)</b><br>' . number_format($order->weaving_per_meter, 2) . '</td>
+</tr>
+<tr>
+    <td><b>GSM</b><br>' . number_format($order->gsm, 2) . '</td>
+    <td><b>GSM (Kg/m)</b><br>' . number_format($order->gsm_kg, 4) . '</td>
+    <td colspan="4"><b>Output Greige</b><br>' . e($order->greigeProduct->name ?? 'Not specified') . '</td>
+</tr>
+</table>';
 
         $pdf->writeHTML($specsHtml, true, false, false, false, '');
         $pdf->Ln(3);
