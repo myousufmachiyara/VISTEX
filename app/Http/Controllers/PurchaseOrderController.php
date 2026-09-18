54cf869974a5912126c62bde8ac91a8c4bddc6f1
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-18 15:54:15 +0500

    Update PurchaseOrderController.php

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -511,7 +511,7 @@ class PurchaseOrderController extends Controller
 
         <tr>
             <td width="17%" style="border:0.75px solid #000;background-color:#f0f0f0;"><b>Fabric Quality</b></td>
-            <td style="border:0.75px solid #000;">' . e($quality) . '</td>
+            <td width="83%" style="border:0.75px solid #000;">' . e($quality) . '</td>
         </tr>
 
         <tr style="background-color:#f0f0f0;">
